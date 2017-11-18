fc7249199d17a0cd3bbc24662f3601fb4c6123d4
xinitrc <xinitrc@x-hub.co>
2017-11-18 03:46:43 -0500

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='prettytwitch',
+    version='0.0.1',
+    description='Package for easy identification of IRC Twitch responses, as well as sending requests.',
+    license='MIT',
+    packages=['prettytwitch'],
+    author='Alexander Berdichevskiy',
+    author_email='xinitrc@x-hub.co',
+    keywords=['twitch', 'irc', 'pretty', 'prettytwitch', 'prettyirc', 'twitchbot'],
+    url='https://github.com/xinitrc-ls/prettytwitch'
+)
